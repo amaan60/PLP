d2729f09aa8ad8aa225511388cd67e138da496a3
amaan60 <51753694+amaan60@users.noreply.github.com>
2020-05-15 12:30:11 +0530

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,9 @@
+insert into booking values(123212.00, '2020-05-13', 1456.00, 'amaan.ahmad7@gmail.com','Good Service' ,5, '1A 2B',2,900);
+insert into booking values(343224.00, '2020-05-14', 3456.00, 'yhad2.hsiva7@gmail.com', null ,4, '1A 2B',2,1900);
+insert into booking values(489835.00, '2020-05-12', 5439.00, 'shivam.singh@gmail.com','On time' ,4, '1A 2B 2A',3, 2700);
+insert into booking values(623242.00, '2020-05-13', 1456.00, 'amaan.ahmad7@gmail.com','Good' ,5, '3A',1,900);
+insert into booking values(324252.00, '2020-05-14', 1456.00, 'yash.abhya7@gmail.com','Bad Service' ,4 ,'4A 5B',2,1800);
+insert into booking values(723614.00, '2020-05-12', 1456.00, 'shivani.asda7@gmail.com', null ,4, '6A',1,900);
+insert into booking values(324272.00, '2020-05-11', 1233.00, 'amaan.ahmad7@gmail.com', null ,5, '1A',1,1500);
+insert into booking values(5262127.00, '2020-05-13', 1233.00, 'adeeb.ahmad@gmail.com', null ,5, '2A',1,1500);
+insert into booking values(423213.00, '2020-05-10', 1456.00, 'amaan.ahmad7@gmail.com', null ,5, '7A',1,900);
